9ada43fdc4785c9d764fda6f4eb5d91dd7cd0289
Bilal Hassan <bilal_hassan@live.com>
2025-03-12 23:09:08 +0300

    feat(develop): further domain building [minor]

---
+++ LocalAuthorityDistricts.Domain/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalAuthorityDistricts.Domain
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
