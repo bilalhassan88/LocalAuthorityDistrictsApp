e7a214b2769b64e27f89bcdc5f33267c941b7f36
Bilal Hassan <bilal_hassan@live.com>
2025-03-15 17:21:10 +0300

    feat(develop): struct update [minor]

---
+++ LocalAuthorityDistricts.Domain/Entities/Geometries/MultiLineStringCoordinates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalAuthorityDistricts.Domain
+{
+    public class MultiLineStringCoordinates : ICoordinates
+    {
+        public List<List<List<double>>> Coordinates { get; private set; }
+
+        public MultiLineStringCoordinates(List<List<List<double>>> coordinates)
+        {
+            Coordinates = coordinates ?? throw new ArgumentNullException(nameof(coordinates), "MultiLineString coordinates cannot be null");
+        }
+    }
+}
--- LocalAuthorityDistricts.Domain/Entities/Geometries/PointCoordinates.cs
+++ LocalAuthorityDistricts.Domain/Entities/Geometries/PointCoordinates.cs
@@ -1,12 +1,11 @@
-﻿//namespace LocalAuthorityDistricts.Domain
-//{
-//    public class PointCoordinates : ICoordinates
-//    {
-//        public List<double> Coordinates { get; set; }
+﻿using LocalAuthorityDistricts.Domain;
 
-//        public PointCoordinates(List<double> coordinates)
-//        {
-//            Coordinates = coordinates ?? new List<double> { 0.0, 0.0 };
-//        }
-//    }
-//}
+public class PointCoordinates : ICoordinates
+{
+    public List<double> Coordinates { get; private set; }
+
+    public PointCoordinates(List<double> coordinates)
+    {
+        Coordinates = coordinates ?? throw new ArgumentNullException(nameof(coordinates), "Point coordinates cannot be null");
+    }
+}
