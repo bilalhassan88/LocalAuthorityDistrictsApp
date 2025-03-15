90c8e5e6bd2f8c5fc4a9939c6cd0b74959217f45
Bilal Hassan <bilal_hassan@live.com>
2025-03-15 23:38:12 +0300

    feat(develop): updated code [minor]

--- LocalAuthorityDistricts.Presentation.BlazorServer/wwwroot/js/cityDropdown.js
+++ LocalAuthorityDistricts.Presentation.BlazorServer/wwwroot/js/cityDropdown.js
@@ -1,9 +1,5 @@
-﻿// wwwroot/js/cityDropdown.js
-
-// Define getSelectedCities globally
-window.getSelectedCities = function (selector) {
+﻿window.getSelectedCities = function (selector) {
     const $dropdown = $(selector);
-    // Ensure Select2 is initialized
     if (!$dropdown.hasClass('select2-hidden-accessible')) {
         $dropdown.select2();
     }
@@ -14,12 +10,10 @@ window.initializeCityDropdown = (cities) => {
     const $dropdown = $("#cityDropdown");
     $dropdown.empty();
 
-    // Add cities as options
     cities.forEach(city => {
         $dropdown.append(new Option(city.name, city.id, false, false));
     });
 
-    // Initialize Select2
     $dropdown.select2({
         placeholder: "Select cities",
         allowClear: true,
